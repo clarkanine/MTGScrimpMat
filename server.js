b616d0fcefad32f1bbd4feee8251a5bfe9111b6d
Aaron Clark <aclark56@eagles.ewu.edu>
2018-06-09 02:46:36 -0600

    fixing mongo on deploy

--- server.js
+++ server.js
@@ -1,22 +1,6 @@
-
 const path = require('path');
 const express = require('express');
 const app = express();
-
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
-
-var db;
-
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-db = client.db();
-console.log("Database connection ready");
 
 // If an incoming request uses
 // a protocol other than HTTPS,
