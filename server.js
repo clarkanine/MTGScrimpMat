673cb5c25d2198bf21e0ec77a9bd128efde024ed
Aaron Clark <aclark56@eagles.ewu.edu>
2018-06-09 02:19:56 -0600

    fixing mongo on deploy

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
-
-var express = require('express'),
-app = express(),
-port = process.env.PORT || 4201,
-mongoose = require('mongoose'),
-Task = require('./api/models/todoListModel'), //created model loading here
-Player = require('./api/models/playerModel'), 
-path = require("path"),
-bodyParser = require('body-parser');
-
-// const path = require('path');
-// const express = require('express');
-// const app = express();
+const path = require('path');
+const express = require('express');
+const app = express();
 
 // If an incoming request uses
 // a protocol other than HTTPS,
@@ -44,35 +34,42 @@ app.get('/*', function(req, res) {
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
-// const express = require('express');
-// const app = express();
-// const path = require('path');
-// app.use(express.static('./dist/mtgscrimpmat'));
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join('./dist/mtgscrimpmat/index.html'));
-// });
-// app.listen(process.env.PORT || 8080);
+// // // const express = require('express');
+// // // const app = express();
+// // // const path = require('path');
+// // // app.use(express.static('./dist/mtgscrimpmat'));
+// // // app.get('/*', function(req, res) {
+// // //   res.sendFile(path.join('./dist/mtgscrimpmat/index.html'));
+// // // });
+// // // app.listen(process.env.PORT || 8080);
 
-
+// // var express = require('express'),
+// //   app = express(),
+// //   port = process.env.PORT || 4201,
+// //   mongoose = require('mongoose'),
+// //   Task = require('./api/models/todoListModel'), //created model loading here
+// //   Player = require('./api/models/playerModel'), 
+// //   path = require("path"),
+// //   bodyParser = require('body-parser');
   
-// mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017'); 
+// // // mongoose instance connection url connection
+// // // mongoose.Promise = global.Promise;
+// // // mongoose.connect('mongodb://localhost:27017'); 
 
-// var path = require("path");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-var routes = require('./api/routes/playerRoutes'); //importing route
-// var routes = require('./api/modules.js'); //importing route
-routes(app); //register the route
-
-app.listen(port);
+// // // var path = require("path");
+// // app.use(bodyParser.urlencoded({ extended: true }));
+// // app.use(bodyParser.json());
 
 
-console.log('todo list RESTful API server started on: ' + port);
+// // var routes = require('./api/routes/playerRoutes'); //importing route
+// // // var routes = require('./api/modules.js'); //importing route
+// // routes(app); //register the route
 
-app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + ' not found'})
-  });
+// // app.listen(port);
+
+
+// // console.log('todo list RESTful API server started on: ' + port);
+
+// // // app.use(function(req, res) {
+// // //     res.status(404).send({url: req.originalUrl + ' not found' + req.url + req.})
+// // //   });
