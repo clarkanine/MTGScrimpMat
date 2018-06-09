f0955343a073fa93bdd9972d700374baf545864e
Aaron Clark <aclark56@eagles.ewu.edu>
2018-06-08 21:05:39 -0600

    added path to server.js

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ var express = require('express'),
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'), //created model loading here
   Player = require('./api/models/playerModel'), 
+  path = require("path"),
   bodyParser = require('body-parser');
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017'); 
 
-
+// var path = require("path");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
