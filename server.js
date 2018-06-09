18eb4abdbff38f62e69c2357bd42c4b7c96dea5e
Aaron Clark <aclark56@eagles.ewu.edu>
2018-06-09 02:36:39 -0600

    fixing mongo on deploy

--- server.js
+++ server.js
@@ -1,50 +1,3 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const Task = require('./api/models/todoListModel'); //created model loading here
-const Player = require('./api/models/playerModel'); 
-const path = require("path");
-const bodyParser = require('body-parser');
-
-// mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017'); 
-
-var routes = require('./api/routes/playerRoutes'); //importing route
-
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    next();
-  }
-}
-
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
-
-// Run the app by serving the static files
-// in the dist directory
-console.log(__dirname);
-app.use(express.static(__dirname + '/dist/MtgScrimpMat'));
-
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/MtgScrimpMat/index.html'));
-});
-
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 4201);
 // // // const express = require('express');
 // // // const app = express();
 // // // const path = require('path');
@@ -84,3 +37,70 @@ app.listen(process.env.PORT || 4201);
 // // // app.use(function(req, res) {
 // // //     res.status(404).send({url: req.originalUrl + ' not found' + req.url + req.})
 // // //   });
+
+// //   var distDir = __dirname + "/src/";
+// //   app.use(express.static(distDir));
+
+// //   app.get('*', function(req, res) {
+// //     const index = path.join(__dirname, 'dist', 'index.html');
+// //     res.sendFile(index);
+// //   })
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
+// // ForceSSL middleware
+// app.use(forceSSL());
+// app.use(express.static(__dirname + '/dist'));
+// app.listen(process.env.PORT || 8080);
+
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    next();
+  }
+}
+
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+// Run the app by serving the static files
+// in the dist directory
+console.log(__dirname);
+app.use(express.static(__dirname + '/dist/MtgScrimpMat'));
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/MtgScrimpMat/index.html'));
+});
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
