78cb1b067f28bbd7aa2693e880d802d7fd4c19de
Aaron Clark <aclark56@eagles.ewu.edu>
2018-06-08 20:30:03 -0600

    different app.use

--- server.js
+++ server.js
@@ -36,8 +36,7 @@ app.listen(port);
 console.log('todo list RESTful API server started on: ' + port);
 
 app.use(function(req, res) {
-    res.status(404).send({url: req + ' not found'})
-    console.log(req);
+    res.status(404).send({url: req.originalUrl + ' not found' + req.url})
   });
 
 
