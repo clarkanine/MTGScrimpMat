20064cd7a37e9434a826e888a8792956a58806d7
Aaron Clark <aclark56@eagles.ewu.edu>
2018-06-09 12:25:09 -0600

    trying to add mongo

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ const forceSSL = function() {
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017'); 
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/playerRoutes'); //importing route
+// var routes = require('./api/modules.js'); //importing route
+routes(app); //register the route
+
 // Instruct the app
 // to use the forceSSL
 // middleware
